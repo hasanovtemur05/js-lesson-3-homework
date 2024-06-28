bffa9067f6a86529b622e6810621b5c7b75d311e
hasanovtemur05 <hasanovtemur86@gmail.com>
2024-06-28 10:20:37 +0500

    added about

---
+++ masala.js
@@ -0,0 +1,59 @@
+// 1 - masala
+// let number1 = prompt("enter your number-1")
+// let number2 = prompt("enter your number-2")
+// let number3 = prompt("enter your number-3")
+// console.log(number1);
+// console.log(number2);
+// console.log(number3);
+// if (number1 >= number2 && number1 >= number3) {
+//     console.log("eng katta", number1);
+// } else if (number2 >= number1 && number1 >= number3) {
+//     console.log("eng katta", number2);
+// } else {
+//     console.log("eng katta", number3);
+// }
+
+
+
+// 2 - masala
+// let son = prompt("son kiriting")
+
+// if (son >= 1000) {
+//     console.log("lux");
+// } else if (son >= 500 && son < 1000) {
+//     console.log("yarim lux");
+// } else if (son >= 200 && son < 500) {
+//     console.log("oddiy xona");
+// } else {
+//     console.log("joy yuq");
+// }
+
+
+
+// 3 - masala
+// let son = prompt("son kirirting")
+// if (son % 3 == 0) {
+//     console.log("3 ga bo'linadi");
+// }else if (son % 5 == 0) {
+//     console.log("5 ga bo'linadi");
+// }else {
+//     console.log("3 ga ham 5 ga ham bo'linmaydi");
+// }
+
+
+
+// 4 - masala
+// let son1 = prompt("Birinchi sonni kiriting:");
+// let son2 = prompt("Ikkinchi sonni kiriting:");
+// let son3 = prompt("Uchinchi sonni kiriting:");
+// console.log(son1);
+// console.log(son2);
+// console.log(son3);
+
+// if (son1 <= son2 && son1 <= son3) {
+//    console.log("eng kichigi", son1);
+// } else if (son2 <= son1 && son2 <= son3) {
+//     console.log("eng kichigi", son2);
+// } else {
+//     console.log("eng kichigi", son3);
+// }
